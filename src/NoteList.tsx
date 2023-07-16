7bac9c999b587ee3a76004859a6ffc4b787d8418
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-07-16 18:15:37 +0100

    creating notes field + local storage + tags

---
+++ src/NoteList.tsx
@@ -0,0 +1,11 @@
+import { Col, Row } from "react-bootstrap";
+
+
+export function NoteList() {
+    return <>
+    <Row>
+        <Col><h1>Notes</h1></Col>
+        <Col xs="auto"></Col>
+    </Row>
+    </>
+}
