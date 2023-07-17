dc22070c56198da47df2b973d0fb84993990a6b5
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-07-17 16:18:42 +0100

    edit/delete tags modal-final commit

---
+++ src/Note.tsx
@@ -0,0 +1,31 @@
+import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
+import { useNote } from "./NoteLayout";
+import { Link } from "react-router-dom";
+
+export function Note() {
+    const note = useNote()
+
+    return <>
+    <Row className="align-items-center mb-4">
+        <Col>
+        <h1>{note.title}</h1>
+        {note.tags.length > 0 && (
+             <Stack gap={1} direction="horizontal" className="flex-wrap">
+
+                   {note.tags.map(tag => (
+                    <Badge className="text-truncate" key={tag.id}>{tag.label} 
+                    </Badge>
+                   ))} 
+                    </Stack>)}
+        </Col>
+         <Col xs="auto">
+        <Stack gap={2} direction="horizontal">
+        <Link to="new">
+            <Button variant="primary">Create</Button>
+        </Link>
+        <Button variant="outline-secondary">Edit Tags</Button>
+        </Stack>
+        </Col>
+    </Row>
+    </>
+}
